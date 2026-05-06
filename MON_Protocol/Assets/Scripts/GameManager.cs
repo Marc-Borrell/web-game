690e93a4b844a086517f0f46293254a36e059aca
marcboso2005 <marcboso2005@gmail.com>
2026-05-06 20:06:03 +0200

    script to connect login in Angular with game

---
+++ MON_Protocol/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Globalization;
+
+public class GameManager : MonoBehaviour
+{
+    private string authToken;
+
+    // Angular llama a este método via SendMessage
+    public void SetAuthToken(string token)
+    {
+        authToken = token;
+        Debug.Log("Token recibido: " + token);
+    }
+
+    // Ejemplo: guardar score al acabar un nivel
+   /* public void SaveScore(int levelId, int moves, int timeMs)
+    {
+        StartCoroutine(PostScore(levelId, moves, timeMs));
+    } 
+
+    private IEnumerator PostScore(int levelId, int moves, int timeMs)
+    {
+        var body = new ScorePayload { level_id = levelId, moves = moves, time_ms = timeMs };
+        string json = JsonUtility.ToJson(body);
+
+        using var request = new UnityWebRequest("https://tu-api.com/scores", "POST");
+        request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json));
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Bearer " + authToken); // JWT
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+            Debug.Log("Score guardado: " + request.downloadHandler.text);
+        else
+            Debug.LogError("Error: " + request.error);
+    } */
+}
+
+[System.Serializable]
+public class ScorePayload
+{
+    public int level_id;
+    public int moves;
+    public int time_ms;
+} 
+
+
+[System.Serializable]
+public class AuthResponse
+{
+    public string msg;
+    public string token;
+    public User user;
+}
+
+[System.Serializable]
+public class User
+{
+    public int id;
+    public string name;
+    public string email;
+} 
