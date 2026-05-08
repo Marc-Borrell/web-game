71ae7feed3820b64607e7f12e9aeed5eae00decb
Firulais215 <cesar_cordero21@hotmail.com>
2026-05-08 18:34:13 +0200

    GameUpdate

--- web/frontend/public/unity/Build/juegoxd.loader.js
+++ web/frontend/public/unity/Build/juegoxd.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 101618728,
+    wasmFileSize: 101672016,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
