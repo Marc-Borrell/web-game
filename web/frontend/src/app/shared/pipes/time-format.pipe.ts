a64894e5d8850fec25ca8d405f95c5005b39599f
marcboso2005 <marcboso2005@gmail.com>
2026-05-11 20:31:34 +0200

    ranking implemented

--- web/frontend/src/app/shared/pipes/time-format.pipe.ts
+++ web/frontend/src/app/shared/pipes/time-format.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timeFormat'
+  name: 'TimeFormatPipe',
+  standalone: true,
 })
 export class TimeFormatPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(time_ms: number): string {
+    const minuts = Math.floor((time_ms / 60000));
+    const segons = Math.floor((time_ms % 60000) / 1000);
+    return `${String(minuts).padStart(2, '0')}:${String(segons).padStart(2, '0')}`;
   }
 
 }
