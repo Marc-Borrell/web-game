f5caf499019f8b1139d44a767637840f6a330fed
marcboso2005 <marcboso2005@gmail.com>
2026-05-11 18:25:49 +0200

    home page modify

--- web/frontend/src/app/pages/home/home.component.ts
+++ web/frontend/src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { Auth } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar.component';
 import { Footer } from '../../shared/components/footer/footer.component';
-import { UnityService } from '../../core/services/unity.service';
 
 @Component({
   selector: 'app-home',
@@ -11,58 +10,13 @@ import { UnityService } from '../../core/services/unity.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class Home implements OnInit {
+export class Home {
 
-  constructor(private unityService: UnityService) {
-
-  }
 
   private authService = inject(Auth);
   private router = inject(Router);
-
   public usuari: String = this.authService.getUser().name;
 
 
-  ngOnInit() {
-
-    if (this.unityService.getInstance()) return;
-    const token = this.authService.getToken(); // el JWT que guardas en localStorage/sessionStorage
-
-    const username = this.authService.getUser().name;
-
-    //@ts-ignore
-    createUnityInstance(document.querySelector("#unity-canvas"), {
-      dataUrl: "/unity/Build/juegoxd.data",
-      frameworkUrl: "/unity/Build/juegoxd.framework.js",
-      codeUrl: "/unity/Build/juegoxd.wasm",
-      streamingAssetsUrl: "StreamingAssets",
-      companyName: "QQClan",
-      productName: "NOM-Protocol",
-      productVersion: "1.0"
-    }).then((unityInstance: any) => {
-
-      //this.unityService.setInstance(unityInstance);
-
-      const payload = JSON.stringify({ token, username });
-
-      (window as any).onUnityReady = () => {
-        this.unityService.sendMessage('GameManager', 'SetAuthToken', payload);
-      };
-      setTimeout(() => {
-        this.unityService.sendMessage('GameManager', 'SetAuthToken', payload);
-      }, 3000);
-
-      setTimeout(() => {
-        this.unityService.sendMessage('GameManager', 'SetAuthToken', payload);
-
-        // Si hay un nivel pendiente, lo cargamos
-        if (this.unityService.pendingLevel) {
-          this.unityService.sendMessage('GameManager', 'LoadLevel', this.unityService.pendingLevel);
-          this.unityService.pendingLevel = null;
-        }
-      }, 3000);
-    });
-
-  }
 
 }
